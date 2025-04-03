b5c458d64268216fee186759e38be1de43bf654d
Jignesh Patel <jignesh@linksture.com>
2025-04-03 12:17:10 +0530

    mission section

--- wp-content/themes/think-appart/js/main.js
+++ wp-content/themes/think-appart/js/main.js
@@ -92,6 +92,8 @@
 
 		moduleProcess();
 
+		moduleProcessMisson();
+
 		moduleReview();
 
 		projectSinglePage();
@@ -577,6 +579,38 @@
 		})
 
 		function changeIndex($textContainer, index){
+			$textContainer.removeClass('active');
+			$textContainer[index].classList.add('active');
+		}
+	}
+
+	function moduleProcessMisson(){
+		$('.module-process .process-container-misson').each(function(i, module){
+			var $module = $(module);
+			var $textContainer = $module.find('.text-container');
+			var changeActive = false;
+
+			var tween = new TimelineMax();
+
+			$textContainer.each(function(i, item){
+				tween.fromTo($(item).find('.line'), 1, {height: '0px'}, {height: '0px', ease: Linear.easeNone, onComplete: function(){
+						changeIndex($textContainer, Math.min(i+1, $textContainer.length-1));
+				}, onReverseComplete: function(){
+						changeIndex($textContainer, Math.max(i-1, 0));
+				}})
+			})
+
+			var scene = new ScrollMagic.Scene({
+				triggerElement: module,
+				triggerHook: .7,
+				duration: $module.innerHeight()
+			})
+			.setTween(tween)
+			.addTo(scrollMagicController)
+		})
+
+		function changeIndex($textContainer, index){
+			console.log( index );
 			$textContainer.removeClass('active');
 			$textContainer[index].classList.add('active');
 		}
